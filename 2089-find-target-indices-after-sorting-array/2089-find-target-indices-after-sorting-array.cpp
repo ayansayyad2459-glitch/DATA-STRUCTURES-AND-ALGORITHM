354ae39af1ef4ae6599512fa399b8033cab24f69
ayansayyad2459-glitch <ayansayyad2459@gmail.com>
2026-07-13 00:02:48 +0530

    Time: 0 ms (100%), Space: 15.2 MB (15.01%) - LeetHub

--- 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.cpp
+++ 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.cpp
@@ -1,13 +1,35 @@
 class Solution {
 public:
+    // optimal way without sorting
     vector<int> targetIndices(vector<int>& nums, int target) {
+        int smaller = 0;
+        int equal = 0;
         vector<int>ans;
-        sort(nums.begin(),nums.end());
         for(int i=0;i<nums.size();i++){
-            if(nums[i]==target){
-                ans.push_back(i);
+            if(nums[i]<target){
+                smaller++;
             }
+            else if(nums[i]==target){
+                equal++;
+            }
+        }
+        for(int i=0;i<equal;i++){
+            ans.push_back(smaller + i);
         }
         return ans;
     }
 };
+
+// // class Solution {
+// public:
+//     vector<int> targetIndices(vector<int>& nums, int target) {
+//         vector<int>ans;
+//         sort(nums.begin(),nums.end());
+//         for(int i=0;i<nums.size();i++){
+//             if(nums[i]==target){
+//                 ans.push_back(i);
+//             }
+//         }
+//         return ans;
+//     }
+// };
