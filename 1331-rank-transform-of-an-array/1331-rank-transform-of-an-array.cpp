ca0c8c524623c21e70fcb31dd5a2658b3637395a
ayansayyad2459-glitch <ayansayyad2459@gmail.com>
2026-07-12 23:25:34 +0530

    Time: 35 ms (88.64%), Space: 43.9 MB (55.31%) - LeetHub

--- 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.cpp
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.cpp
@@ -5,12 +5,14 @@ public:
         sort(temp.begin(),temp.end());
         unordered_map<int, int>mpp;
         int rank = 1;
+        // mapping the rank
         for(int i=0;i<temp.size();i++){
           if(mpp.find(temp[i])==mpp.end()){
               mpp[temp[i]]= rank;
           rank++;
            }
         }
+        // replacing the array with their rank
         for(int i = 0;i<arr.size();i++){
             arr[i]= mpp[arr[i]];
         }
