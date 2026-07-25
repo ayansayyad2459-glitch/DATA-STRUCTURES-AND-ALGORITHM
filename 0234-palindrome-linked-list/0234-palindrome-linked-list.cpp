c301ef7a3939fa7d4ee3a3eddb3b3fc2881ee60d
ayansayyad2459-glitch <ayansayyad2459@gmail.com>
2026-07-25 20:38:55 +0530

    Time: 2 ms (63.21%), Space: 121.9 MB (43.3%) - LeetHub

---
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -0,0 +1,50 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+ // Reverse a linked list
+    ListNode* reverse(ListNode* head){
+        ListNode* prev = NULL;
+        ListNode* curr = head;
+
+        while(curr != NULL){
+            ListNode* front = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = front;
+        }
+
+        return prev;
+    }
+    bool isPalindrome(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast->next != NULL && fast->next->next != NULL){
+            // finding middle
+            slow = slow->next;
+            fast = fast ->next->next;
+        }
+        ListNode* newHead = reverse(slow->next);
+        ListNode* first = head;
+        ListNode* second = newHead;
+        // reversing and comparing
+        while(second!=NULL){
+            if(first->val != second->val){
+                reverse(newHead);
+                return false;
+            }
+            first = first->next;
+            second= second->next;
+        }
+        reverse(newHead);
+        return true;
+    }
+};
