11bcf0684a57e753876c206bc36d1479970835d6
ayansayyad2459-glitch <ayansayyad2459@gmail.com>
2026-09-11 20:26:30 +0530

    Time: 45 ms (48.09%), Space: 19.3 MB (45.08%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        unordered_map<char, int>mpp;
+
+        int left=0;
+        int ans=0;
+        for(int right= 0; right< s.size();right++){
+
+            if(mpp.find(s[right]) != mpp.end()){
+                left = max(left, mpp[s[right]]+1);
+            }
+            mpp[s[right]]= right;
+
+            int len = right-left+1;
+            ans = max(ans,len);
+        }
+        return ans;
+    }
+};
